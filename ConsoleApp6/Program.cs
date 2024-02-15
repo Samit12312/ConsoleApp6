f060143f379ddaf8a9e7ff2e237ae681b6ce94cb
Samit12312 <154971416+Samit12312@users.noreply.github.com>
2024-02-15 22:20:37 +0200

    Update Program.cs

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -129,11 +129,11 @@ namespace ConsoleApp6
                 Console.WriteLine("there are no 3 letters");
             }
 
-    char char1 = input[0];
-    char char2 = input[1];
-    char char3 = input[2];
-    int chack = (char)(char1 + 1);
-            if ((char2 == (char) (char1 + 1)) && (char3 == (char) (char2 + 1)))
+            char char1 = input[0];
+            char char2 = input[1];
+            char char3 = input[2];
+            int chack = (char)(char1 + 1);
+            if ((char2 == (char)(char1 + 1)) && (char3 == (char)(char2 + 1)))
             {
                 Console.WriteLine("the characters follow the alpha bet");
                 Console.WriteLine(char1);
@@ -142,9 +142,9 @@ namespace ConsoleApp6
                 Console.WriteLine((char)(char3 + 1));
             }
             else
-{
-    Console.WriteLine("the characters arent in order of the alpha bet");
-}
+            {
+                Console.WriteLine("the characters arent in order of the alpha bet");
+            }
         }
         static void targil8()
         {
@@ -164,29 +164,63 @@ namespace ConsoleApp6
             else Console.WriteLine("num3 is the biggest");
 
         }
-        static void Main(string[] args)
+        static void targil9()
         {
             Console.WriteLine("enter 3 length of a tringle");
+
             int a = int.Parse(Console.ReadLine());
             int b = int.Parse(Console.ReadLine());
             int c = int.Parse(Console.ReadLine());
-            if (a + b < c || b + c < a || c + a < b)
+
+            if (a + b <= c || b + c <= a || c + a <= b)
             {
-                Console.WriteLine("cannot create tringle");
+                Console.WriteLine("cannot create triangle");
             }
-            else if (a ==  b)
+            else if (a == b && b == c)
             {
-                Console.WriteLine("trinsle is an isosceles");
+                Console.WriteLine("triangle is an equilateral triangle");
             }
-            else if (a == b && c == a || b == c)
+            else if (a == b || b == c || c == a)
             {
-                Console.WriteLine("equilateral");
+                Console.WriteLine("triangle is an isosceles triangle");
+            }
+            else if (a * a + b * b == c * c || b * b + c * c == a * a || c * c + a * a == b * b)
+            {
+                Console.WriteLine("its a right triangle");
+            }
+            else
+            {
+                Console.WriteLine("this is a triangle");
             }
 
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a three-digit number:");
+            int number = int.Parse(Console.ReadLine());
 
+            int num1 = number % 10;
+            int num2 = number / 10 % 10;
+            int num3 = number / 100;
 
+            if (num1 % 2 != 0)
+            {
+                num1 -= 1;
+            }
 
-            Console.ReadLine();
+            if (num2 % 2 != 0)
+            {
+                num2 -= 1;
+            }
+
+            if (num3 % 2 != 0)
+            {
+                num3 -= 1;
+            }
+
+            int encryptedCode = num3 * 100 + num2 * 10 + num1;
+            Console.WriteLine("encrypted code: " + encryptedCode);
+            Console.ReadLine();           
         }
     }
 }
